de2b12506e4dfdcde279193316e47e4f02452dea
d3an <jabury@sympatico.ca>
2019-12-07 17:48:19 -0500

    minor wasm update

--- Makefile
+++ Makefile
@@ -4,11 +4,19 @@ OBJECTS=SimpleMCMain5.o SimpleMC2.o Random1.o PayOff2.o DoubleDigital.o
 DEPENDS=${OBJECTS:.o=.d}
 EXEC=montecarlo
 
+OBJECTS1=SimpleMCMain2.o SimpleMC.o Random1.o PayOff1.o
+OBJECTS2=SimpleMCMain5.o SimpleMC2.o Random1.o PayOff2.o DoubleDigital.o
+
+override OBJECTS = ${OBJECTS1}
+
+ALL_OBJECTS=${OBJECTS1} ${OBJECTS2}
+ALL_DEPENDS=${ALL_OBJECTS:.o=.d}
+
 ${EXEC}: ${OBJECTS}
 	${CXX} ${CXXFLAGS} ${OBJECTS} -o ${EXEC}
 
 -include ${DEPENDS}
 
-clean:
-	rm ${OBJECTS} ${DEPENDS} ${EXEC}
 .PHONY: clean
+clean:
+	rm ${ALL_OBJECTS} ${ALL_DEPENDS} ${EXEC}
--- SimpleMCMain2.cpp
+++ SimpleMCMain2.cpp
@@ -9,6 +9,26 @@
 #include <iostream>
 using namespace std;
 
+double convert_days_to_years(double days) {
+    return days / 365.0;
+}
+
+extern "C"
+double calculateCallPayoff(double Expiry, double Strike, double Spot, double Vol, double r, unsigned long NumberOfPaths) {
+    PayOff callPayOff(Strike, PayOff::call);
+    Expiry = convert_days_to_years(Expiry);
+    double resultCall = SimpleMonteCarlo2(callPayOff, Expiry, Spot, Vol, r, NumberOfPaths);
+    return resultCall;
+}
+
+extern "C"
+double calculatePutPayoff(double Expiry, double Strike, double Spot, double Vol, double r, unsigned long NumberOfPaths) {
+    PayOff putPayoff(Strike, PayOff::put);
+    Expiry = convert_days_to_years(Expiry);
+    double resultPut = SimpleMonteCarlo2(putPayoff, Expiry, Spot, Vol, r, NumberOfPaths);
+    return resultPut;
+}
+
 int main() {
   double Expiry;
   double Strike;
@@ -19,6 +39,7 @@ int main() {
 
   cout << "\nEnter expiry\n";
   cin >> Expiry;
+  Expiry = convert_days_to_years(Expiry);
 
   cout << "\nEnter strike\n";
   cin >> Strike;
